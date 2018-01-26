a5a32d96f1b78e5173643c696233ebb58f6226e8
Joseph Melika <jmelika@hotmail.com>
2018-01-25 21:03:13 -0800

    added eth to btc

---
+++ sfox_ethbtc.2s.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+export PATH='/usr/local/bin:/usr/bin:$PATH'
+
+# Shows (Ask, Bid, Mid, High, Low, Volume and Timestamp from Bitfinex REST API Ticker BTCUSD 
+#
+# <bitbar.title>SFOX ARB BTCUSD plugin</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Joseph Melika</bitbar.author>
+# <bitbar.author.github>jmelika</bitbar.author.github>
+# <bitbar.desc>Shows (Ask, Bid and ARB/SPREAD) </bitbar.desc>
+# <bitbar.image></bitbar.image>
+#
+# by Joseph Melika
+
+SFOX_FEE="0.007"
+
+DELTA=$(curl -s "https://api.sfox.com/v1/markets/orderbook/ethbtc" | jq -r '[.asks[0] | .[0]], .bids[0] | .[0]')
+
+# Let's get some useful values from here
+USD_ASK=`echo $DELTA | awk -F" " '{print $1}'`
+USD_BID=`echo $DELTA | awk -F" " '{print $2}'`
+SFOX_FEE_USD=$(echo "${USD_ASK} * ${SFOX_FEE}" | bc)
+SPREAD_ARB=$(echo "${USD_ASK} - ${USD_BID} + ${SFOX_FEE_USD}" | bc)
+
+
+# I need to make the Arb pop, so let's put some colors
+case "$SPREAD_ARB" in
+-*)
+    echo "EB -$SPREAD_ARB | color=red"
+    tweet.sh post ARB between ETH and BTC at -$SPREAD_ARB
+    ;;
+*)
+    echo "EB $SPREAD_ARB"
+    ;;
+esac
+
+#Let's display the data
+echo "---"
+echo "Ask: " $USD_ASK
+echo "Bid: " $USD_BID
+echo $SPREAD_ARB
+echo "---"
